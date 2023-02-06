08567fa177f4a06ac9c8b7fca8c354298ae5d9bc
Terrence Chung <terrencechung35@gmail.com>
2023-02-05 16:39:11 -0800

    lab

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+
+
+function Filter({ onCategoryChange, onSearchChange, search}) {
+    const handleSearchChange = (event) => {
+      onSearchChange(event.target.value);
+    }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input 
+        type="text" 
+        name="search" 
+        placeholder="Search..."
+        value={search}
+        onChange={handleSearchChange} 
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+
+function ItemForm({onItemFormSubmit}) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      name,
+      category
+    });
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input 
+          type="text"
+          name="name"
+          onChange={handleNameChange}
+          value={name}
+          />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select 
+          name="category"
+          onChange={handleCategoryChange}
+          value={category}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
